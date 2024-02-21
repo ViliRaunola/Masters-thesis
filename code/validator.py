dd790fb47df1ce18c07124b0a20ea5ac3fa8ba01
Vili Raunola <raunolavili@gmail.com>
2024-02-21 17:57:03 +0200

    Validation program done
    Can insert own tsv file that has tags and data. The validator.py will then check how well the pipeline performs on the data.

---
+++ code/validator.py
@@ -0,0 +1,34 @@
+import datasets
+import evaluate
+import models.tagger as tagger
+import numpy as np
+from transformers import TrainingArguments
+
+metric = evaluate.load("seqeval")
+
+my_data_val = tagger._read_coll_file("../own_data/results.tsv")
+lables_to_ids = tagger._create_label_mapping_dics()[0]
+my_dataset = datasets.Dataset.from_pandas(my_data_val)
+
+pipeline = tagger.get_ner_pipeline()
+training_args = TrainingArguments("test_trainer")
+
+task_evaluator = evaluate.evaluator("token-classification")
+
+
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    return metric.compute(predictions=predictions, references=labels)
+
+
+results = task_evaluator.compute(
+    model_or_pipeline=pipeline,
+    data=my_dataset,
+    metric=metric,
+    input_column="tokens",
+    label_column="labels",
+)
+
+for key in results:
+    print(f"For {key}: {results[key]}")
